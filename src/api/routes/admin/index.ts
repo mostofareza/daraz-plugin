1cb3bdc231bddbf808581a9d939c435cca7c480d
saimonMVN <sultan@moveon.com.bd>
2023-09-21 14:57:08 +0600

    refactor price list

--- src/api/routes/admin/index.ts
+++ src/api/routes/admin/index.ts
@@ -3,6 +3,14 @@
 import { ConfigModule, authenticate, wrapHandler } from "@medusajs/medusa";
 import { Request, Response, Router } from "express";
 import cors from "cors";
+import {
+  createPriceSettingsHandler,
+  deletePriceSettingsHandler,
+  getPriceSettingsListHandler,
+  updatePriceSettingsHandler,
+} from "./price-settings";
+import { CreatePriceSettingValidation } from "../../../middlewares/create-price-validation-middlewares";
+import { UpdatePriceSettingValidation } from "../../../middlewares/update-price-validation-middleware";
 
 export default function adminRoutes(router: Router, options: ConfigModule) {
   const { projectConfig } = options;
@@ -18,13 +26,17 @@ export default function adminRoutes(router: Router, options: ConfigModule) {
   adminRouter.use(cors(adminCorsOptions));
   adminRouter.use(authenticate());
 
-  // Define the route using the imported handler function
-  //   adminRouter.get(
-  //     "/test",
-  //     wrapHandler(async (req: Request, res: Response) => {
-  //       // @ts-ignore
-  //       console.log(res);
-  //       res.json("Hello World");
-  //     })
-  //   );
+  // inventory product price role settings
+  router.get("/price-role-settings", getPriceSettingsListHandler);
+  router.post(
+    "/price-role-settings",
+    CreatePriceSettingValidation,
+    createPriceSettingsHandler
+  );
+  router.patch(
+    "/price-role-settings/:id",
+    UpdatePriceSettingValidation,
+    updatePriceSettingsHandler
+  );
+  router.delete("/price-role-settings/:id", deletePriceSettingsHandler);
 }
---
+++ src/api/routes/admin/price-settings/index.ts
@@ -0,0 +1,128 @@
+import { Request, Response } from "express";
+import SettingsService from "../../../../services/settings";
+import { DEFAULT_lIMIT } from "../../../index";
+
+export const getPriceSettingsListHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const InventorySettings: SettingsService =
+    req.scope.resolve("settingsService");
+
+  const { limit, offset, store_slug } = req.query;
+
+  const config: {
+    take: number;
+    skip: number;
+    where?: { store_slug?: string };
+  } = {
+    take: limit ? Number(limit) : DEFAULT_lIMIT,
+    skip: offset ? Number(offset) : 0,
+  };
+
+  if (store_slug && typeof store_slug === "string") {
+    config.where = { store_slug };
+  }
+
+  try {
+    const [response, count] = await InventorySettings.list(config);
+
+    res.status(200).json({
+      result: response,
+      count,
+      limit: config.take,
+      offset: config.skip,
+      filters: [
+        {
+          key: "store_slug",
+          value: "string",
+        },
+      ],
+      statusCode: 200,
+      message: "Retrieve price settings successful",
+    });
+  } catch (error: any) {
+    res.status(error.status || 500).json({
+      error: error,
+    });
+  }
+};
+
+export const createPriceSettingsHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const InventorySettings: SettingsService =
+    req.scope.resolve("settingsService");
+  const data = req.body;
+
+  try {
+    const response = await InventorySettings.create(data);
+    res.status(200).json({
+      result: response,
+      statusCode: 200,
+      message: "successfully created",
+    });
+  } catch (error: any) {
+    res.status(error.status || Number(error.code) || 500).json({
+      error: error,
+    });
+  }
+};
+
+export const updatePriceSettingsHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+
+  if (!id) {
+    return res.status(422).json({
+      error: [
+        {
+          key: "id",
+          message: "id is required and must be a string",
+        },
+      ],
+    });
+  }
+  const InventorySettings: SettingsService =
+    req.scope.resolve("settingsService");
+
+  try {
+    const response = await InventorySettings.update({ id, ...updatedData });
+    res.status(200).json({
+      result: response,
+      statusCode: 200,
+      message: "successfully updated",
+    });
+  } catch (error: any) {
+    res.status(error.status || Number(error.code) || 500).json({
+      error: error,
+    });
+  }
+};
+
+export const deletePriceSettingsHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const id = req.params.id;
+
+  const InventorySettings: SettingsService =
+    req.scope.resolve("settingsService");
+
+  try {
+    const response = await InventorySettings.delete(id);
+    res.status(200).json({
+      result: response,
+      statusCode: 200,
+      message: "successfully deleted",
+    });
+  } catch (error: any) {
+    res.status(error.status || Number(error.code) || 500).json({
+      error: error,
+    });
+  }
+};
