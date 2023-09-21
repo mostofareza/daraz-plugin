2c5d0e71e60f288ebf1b382c35449b4d5248c335
saimonMVN <sultan@moveon.com.bd>
2023-09-21 17:35:18 +0600

    update campain api

--- src/api/routes/store/campain.ts
+++ src/api/routes/store/campain.ts
@@ -38,7 +38,7 @@ export async function getCampaignListHandler(req: Request, res: Response) {
 
 export async function getCampaignProductsHandler(req: Request, res: Response) {
   const id = req.params.id;
-  const { limit, offset } = req.query;
+  const { limit, offset, orderBy, q } = req.query;
 
   if (!id) {
     return res.status(422).json({
@@ -51,21 +51,48 @@ export async function getCampaignProductsHandler(req: Request, res: Response) {
     });
   }
 
+  const searchText = q as string;
+
   const priceListService: PriceListService =
     req.scope.resolve("priceListService");
+
+  const filterableFields: FilterableProductProps = {
+    price_list_id: [id],
+  };
+  if (searchText && searchText.trim()) {
+    filterableFields.q = searchText;
+  }
 
   const config: FindConfig<Product> = {
     take: limit ? Number(limit) : LIMIT,
     skip: offset ? Number(offset) : 0,
     relations: ["variants", "images", "tags", "collection", "options"],
+    order:
+      orderBy && (orderBy === "ASC" || orderBy === "DESC")
+        ? { created_at: orderBy }
+        : { created_at: "ASC" },
   };
 
-  const [products, count] = await priceListService.listProducts(id, {}, config);
+  const [products, count] = await priceListService.listProducts(
+    id,
+    filterableFields,
+    config
+  );
 
   res.json({
     products,
     count,
     offset: config.skip,
     limit: config.take,
+    filters: [
+      {
+        key: "orderBy",
+        value: ["ASC", "DESC"],
+      },
+      {
+        key: "q",
+        value: "string",
+      },
+    ],
   });
 }
